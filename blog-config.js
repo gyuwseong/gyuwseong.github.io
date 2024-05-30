a1f34675a16678259944ea303aeef6ce8e1fd9b6
gyuwseong <gw777seong@gmail.com>
2024-05-30 20:25:44 +0900

    initial commit

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "gyuwseong",
+  description: "Backend developer based in Seoul.",
+  author: "gyuwseong",
+  siteUrl: "https://gyuwseong.github.io",
+  links: {
+    github: "https://github.com/gyuwseong",
+    email: "mailto:gw1231seong@gmail.com",
+  },
+  utterances: {
+    repo: "gyuwseong/gyuwseong.github.io",
+    type: "pathname",
+  },
+};
